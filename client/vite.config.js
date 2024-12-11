36b459b740364fe07d0efcf73f3c4080cd56964f
DhruveshSolanki <dvsolanki21@gmail.com>
2024-12-11 01:45:55 -0500

    DS:trying again to fix host

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 const { PORT = 3000 } = process.env;
 
 export default defineConfig({
