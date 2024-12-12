cd2b0bdf75a477fcae41fd00dbda100015d755b3
DhruveshSolanki <dvsolanki21@gmail.com>
2024-12-11 22:01:11 -0500

    DS: Updated headers in server.js

--- server.js
+++ server.js
@@ -15,7 +15,30 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(compress())
 app.use(helmet())
-app.use(cors({origin: true, credentials: true},))
+app.use(cors({
+    origin: true, 
+    credentials: true,
+    allowedHeaders
+},))
+
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', ['https://farmers-market-e2bc2.web.app','http://localhost:5173']);
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 // Connect to MongoDB
 mongoose.Promise = global.Promise
