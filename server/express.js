5baf35aee35935a4e12b6cac3fda5d8eb9d06051
DhruveshSolanki <dvsolanki21@gmail.com>
2024-12-11 19:21:53 -0500

    DS: Updated express file

--- server/express.js
+++ server/express.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(compress())
 app.use(helmet())
-app.use(cors())
+app.use(cors({origin: true}))
 
 // app.get('/', (req, res) => {
 //     res.status(200).send(Template())
